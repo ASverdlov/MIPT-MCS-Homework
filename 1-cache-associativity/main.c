3fdc3f614d776817da3a58ba491c9ba8d1f57a5c
Albert Sverdlov <sverdlovalbert@gmail.com>
2017-12-23 17:23:17 +0300

    Change average calculation

--- 1-cache-associativity/main.c
+++ 1-cache-associativity/main.c
@@ -12,7 +12,7 @@
 #define INFO GREEN "[BENCHMARK]" RESET
 
 const int MAX_NUMFRAGMENTS = 32;             /* will bench from 1 to MAX_NUMFRAGMENTS */
-const int BUFFER_SIZE = 1024 * 1024 * 1024;  /* 1GB */
+const int BUFFER_SIZE = 32 * 1024 * 1024;    /* 32 Mb */
 char* buffer;
 
 
@@ -23,6 +23,18 @@ int read_envnum(const char* name) {
         exit(1);
     }
     return atoi(value_str);
+}
+
+const char* pretty_memory(int bytes) {
+    char* s = calloc(128, sizeof(char));
+    if (bytes < 1024) {
+        sprintf(s, "%d B", bytes);
+    } else if (bytes < 1024 * 1024) {
+        sprintf(s, "%g KB", (double)bytes / 1024);
+    } else {
+        sprintf(s, "%g MB", (double)bytes / 1024 / 1024);
+    }
+    return s;
 }
 
 /* Returns current time in nanoseconds.
@@ -41,14 +53,7 @@ long long time_ns() {
     return t.tv_nsec + t.tv_sec * 1000LL * 1000 * 1000;
 }
 
-void warmup_cache() {
-    for (int i = BUFFER_SIZE - 1; i >= 0; --i) {
-        (char)buffer[i];
-    }
-    fprintf(stdout, INFO " Cache has been warmed up.\n");
-}
-
-void benchmark(int num_fragments, int fragmentsize) {
+double benchmark_accesstime(int num_fragments, int fragmentsize) {
     long long start_time, end_time;
     long long elapsed_time;
     double access_time;
@@ -59,29 +64,46 @@ void benchmark(int num_fragments, int fragmentsize) {
     start_time = time_ns();
     for (int offset = 0; offset < fragmentsize; ++offset) {
         for (int i = offset; i < fragments_end; i += fragmentsize) {
-            (char)buffer[i + offset];
+            buffer[i] = i;
         }
     }
     end_time = time_ns();
 
     elapsed_time = end_time - start_time;
-    access_time = (double)elapsed_time / fragments_end;
-    fprintf(stdout, BENCHMARK " # fragments = %d\taccess (ns) = %lf\telapsed (ns) = %lld\n",
-        num_fragments, access_time, elapsed_time);
+    return (double)elapsed_time / fragments_end;
 }
 
 int main() {
     /* Read options */
     int FRAGMENT_SIZE = read_envnum("FRAGMENT");
-    fprintf(stdout, INFO " Fragment size: %d bytes\n", FRAGMENT_SIZE);
+    int NUM_WALKS = read_envnum("WALKS");
+    fprintf(stdout, INFO " Fragment size: %s\n", pretty_memory(FRAGMENT_SIZE));
+    fprintf(stdout, INFO " # of walks: %d\n", NUM_WALKS);
 
     /* Allocate the buffer with zeroes */
     buffer = calloc(BUFFER_SIZE, sizeof(char));
-    fprintf(stdout, INFO " Buffer has been allocated with %d bytes\n", BUFFER_SIZE);
+    fprintf(stdout, INFO " Buffer has been allocated with %s\n", pretty_memory(BUFFER_SIZE));
 
-    warmup_cache();
+    /* Results */
+    double sum_accesstime[MAX_NUMFRAGMENTS];
+    for (int i = 0; i < MAX_NUMFRAGMENTS; ++i)
+        sum_accesstime[i] = 0.0;
+
+    /* Benchmark */
+    for (int walk = 0; walk < NUM_WALKS; ++walk) {
+        for (int num_fragments = 1; num_fragments <= MAX_NUMFRAGMENTS; ++num_fragments) {
+            double accesstime = benchmark_accesstime(num_fragments, FRAGMENT_SIZE);
+            sum_accesstime[num_fragments - 1] += accesstime;
+        }
+        fprintf(stdout, BENCHMARK " %d / %d\n", walk, NUM_WALKS);
+        fflush(stdout);
+    }
+
+    /* Print */
     for (int num_fragments = 1; num_fragments <= MAX_NUMFRAGMENTS; ++num_fragments) {
-        benchmark(num_fragments, FRAGMENT_SIZE);
+        double access_time = sum_accesstime[num_fragments - 1] / NUM_WALKS;
+        fprintf(stdout, BENCHMARK " # fragments = %d\taccess (ns) = %0.1lf\n",
+            num_fragments, access_time);
     }
 
     return 0;
